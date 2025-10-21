55cc520fc37b0f1106366b121435fff04b6f43c9
Joao Vitor Dos Santos <joaovitorsanttoos2@gmail.com>
2025-10-21 18:39:36 -0300

    Estudos CRUD java +spring usuarioService adicionado atualizar sem bugs.

--- src/main/java/com/javanauta/cadastro_usuario/infrastrucure/UsuarioRepository.java
+++ src/main/java/com/javanauta/cadastro_usuario/infrastrucure/UsuarioRepository.java
@@ -1,9 +1,15 @@
 package com.javanauta.cadastro_usuario.infrastrucure;
 
 import com.javanauta.cadastro_usuario.infrastrucure.entitys.Usuario;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer>{
 
+    Optional<Usuario> findByEmail (String email);
 
+    @Transactional
+    Void deleteByEmail(String email);
 }
