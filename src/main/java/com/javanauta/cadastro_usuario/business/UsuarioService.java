59d0aec11caf0528c1d3c106f7fe43abf29b3979
Joao Vitor Dos Santos <joaovitorsanttoos2@gmail.com>
2025-10-22 23:12:27 -0300

    Estudos CRUD java +spring usuarioService adicionado atualizar sem bugs.

--- src/main/java/com/javanauta/cadastro_usuario/business/UsuarioService.java
+++ src/main/java/com/javanauta/cadastro_usuario/business/UsuarioService.java
@@ -29,13 +29,17 @@ public class UsuarioService {
         repository.deleteByEmail(email);
     }
 
-    public void atualizarUsuarioPorEmail(String email, Usuario usuario){
-        Usuario usuarioEntity = BuscarUsuarioPorEmail(email);
+    public void atualizarUsuarioPorId(Integer id, Usuario usuario){
+        Usuario usuarioEntity = repository.findById(id).orElseThrow(() ->
+                new RuntimeException("Usuario não encontrado"));
         Usuario usuarioAtualizado = Usuario.builder()
-                .email(usuario.getEmail() != null ?usuario.getEmail() : usuarioEntity.getEmail())
-                .nome(usuario.getNome() !=null ?usuario.getNome() : usuarioEntity.getNome())
+                .email(usuario.getEmail() != null ? usuario.getEmail() :
+                        usuarioEntity.getEmail())
+                .nome(usuario.getNome() != null ? usuario.getNome() :
+                        usuarioEntity.getNome())
                 .id(usuarioEntity.getId())
                 .build();
-                repository.saveAndFlush(usuarioAtualizado);
+
+        repository.saveAndFlush(usuarioAtualizado);
     }
 }
